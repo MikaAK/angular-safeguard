9f4873bf6465a9fc3d38b84fb8e6c74a4fde868a
MikaAK <mikakalathil@gmail.com>
2016-02-18 01:18:13 -0800

    fix typings

---
+++ index.d.ts
@@ -0,0 +1,62 @@
+export interface IStorage {
+  length: number
+  getItem(key: string): any
+  setItem(key: string, data: any): void
+  removeItem(key: string): void
+  clear(): void
+  hasOwnProperty(key: string): boolean
+  key(index: number): string
+}
+
+export class Driver {
+  private storage
+  constructor(storage: IStorage)
+  set(key: string, data: any): void
+  get(key: string): any
+  has(key: string): boolean
+  remove(key: string): void
+  clear(): void
+  key(index?: number): string
+  isSupported(): boolean
+}
+
+
+export class MemoryStorage implements IStorage {
+  hasOwnProperty(key: any): boolean
+  getItem(key: any): any
+  setItem(key: any, value: any): void
+  removeItem(key: any): void
+  clear(): void
+  key(index: any): any
+  length: number
+}
+
+export const DRIVERS: {
+  SESSION: Driver
+  LOCAL: Driver
+  MEMORY: Driver
+}
+
+export class Locker {
+  static DRIVERS: {
+      SESSION: Driver
+      LOCAL: Driver
+      MEMORY: Driver
+  }
+  private driver
+  private namespace
+  constructor({driverNamespace, defaultDriverType}: {
+      driverNamespace: any
+      defaultDriverType?: Driver
+  })
+  setNamespace(namespace?: string): void
+  setDriver(driver: Driver): void
+  set(key: any, data: any, expiry: any): void
+  get(key: any): any
+  has(key: any): boolean
+  remove(key: any): void
+  key(index: any): string
+  clear(): void
+  private _makeKey(key)
+}
+
