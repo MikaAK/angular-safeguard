0c067bdbbb11d6ee2304e4d39f53337de3861bfd
MikaAK <mikakalathil@gmail.com>
2016-02-19 01:35:36 -0800

    Update deps

--- karma.conf.js
+++ karma.conf.js
@@ -61,7 +61,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS2'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
